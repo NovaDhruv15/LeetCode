fac2fd289f6dd7355df828f3ce272eee55f397ca
CodewithDhruv <dhruv0215rawat@gmail.com>
2026-09-18 11:27:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (57.20%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -8,23 +8,19 @@ class Solution:
         right = len(matrix[0]) - 1
 
         while top <= bottom and left <= right:
-            # Left to right
             for i in range(left, right + 1):
                 ans.append(matrix[top][i])
             top += 1
 
-            # Top to bottom
             for i in range(top, bottom + 1):
                 ans.append(matrix[i][right])
             right -= 1
 
-            # Right to left
             if top <= bottom:
                 for i in range(right, left - 1, -1):
                     ans.append(matrix[bottom][i])
                 bottom -= 1
-
-            # Bottom to top
+                
             if left <= right:
                 for i in range(bottom, top - 1, -1):
                     ans.append(matrix[i][left])
