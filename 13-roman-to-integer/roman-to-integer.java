987227a89a6bb8fd71d2d1162a3ee19803b55077
CodewithDhruv <dhruv0215rawat@gmail.com>
2026-09-22 10:28:34 +0530

    Time: 2 ms (100.00%) | Memory: 46.7 MB (35.80%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int romanToInt(String s) {
+        int res = 0, prev = 0, curr = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            switch (s.charAt(i)) {
+                case 'I': curr = 1; break;
+                case 'V': curr = 5; break;
+                case 'X': curr = 10; break;
+                case 'L': curr = 50; break;
+                case 'C': curr = 100; break;
+                case 'D': curr = 500; break;
+                case 'M': curr = 1000; break;
+            }
+            if (curr < prev) {
+                res -= curr;
+            } else {
+                res += curr;
+            }
+            
+            prev = curr;
+        }
+        
+        return res;
+    }
+}
